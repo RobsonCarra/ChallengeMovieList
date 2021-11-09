6f33f07ebd936519d35738a8e8bfc92ca2be3fb3
Robson Simionato Carra <robcarra18@hotmail.com>
2021-11-09 14:57:14 -0300

    retrofit config e interface config

--- app/src/main/java/br/com/alura/ceep/ui/challenge_movielist/helpers/RetrofitConfig.kt
+++ app/src/main/java/br/com/alura/ceep/ui/challenge_movielist/helpers/RetrofitConfig.kt
@@ -1,4 +1,18 @@
 package br.com.alura.ceep.ui.challenge_movielist.helpers
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetrofitConfig {
+    fun getClient(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://mocki.io/v1/ad07ea3d-ce43-49e7-ad58-29dcdef136f5")
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder().build()
+            ).build()
+    }
 }
--- app/src/main/java/br/com/alura/ceep/ui/challenge_movielist/repository/MovieInterface.kt
+++ app/src/main/java/br/com/alura/ceep/ui/challenge_movielist/repository/MovieInterface.kt
@@ -1,4 +1,15 @@
 package br.com.alura.ceep.ui.challenge_movielist.repository
 
+import br.com.alura.ceep.ui.challenge_movielist.domain.Movie
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface MovieInterface {
+    @GET(GET_ALL)
+    fun getAll(): Deferred<Response<List<Movie>>>
+
+    companion object {
+        const val GET_ALL = "v1/ad07ea3d-ce43-49e7-ad58-29dcdef136f5"
+    }
 }
